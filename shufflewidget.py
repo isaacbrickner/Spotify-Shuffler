dc1ae2634fa1b30c342baa8a094c163559c4f927
Isaac Brickner <bricknei@oreegonstate.edu>
2023-04-04 09:09:05 -0700

    build gui and implement button functions. slider for volume doesnt work currently

--- shufflewidget.py
+++ shufflewidget.py
@@ -1,6 +1,6 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from tkinter import *
+import tkinter as tk
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pprint
@@ -12,42 +12,80 @@ import timeit
 import time
 import random
 
-
-
-
 pp = pprint.PrettyPrinter(indent=4)
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="c6d8222701234542ab1296cb5a242c46",
-                                               client_secret="8800442a25954135b6c47a6696661602",
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="482f4b048fb44d07962d5be42f20d1ce",
+                                               client_secret="39f9aec6713c449e84be1e33be8f04f2",
                                                redirect_uri="http://127.0.0.1:9090",
                                                scope="user-top-read user-library-read user-read-currently-playing user-follow-read user-read-playback-state user-modify-playback-state app-remote-control playlist-read-private"))
 
-results = sp.current_user_saved_tracks()
-top_tracks = sp.artist_top_tracks("0oSGxfWSnnOXhD2fKuz2Gy", 'US')
-results = sp.current_user_top_artists(time_range='short_term', limit=20)
-top_artists = sp.current_user_top_artists(20, 0, 'medium_term')
-queue = sp.queue()
-analysis = sp.audio_analysis(
-    'https://open.spotify.com/track/13T8SvWHczyBPzOemKtEe7?si=21d4eefb66184d0d')
-logger = logging.getLogger('examples.artist_recommendations')
-logging.basicConfig(level='INFO')
-
 
 def main():
-    main_window = Tk()
-    main_window.title('Spotify Shuffler')
-    main_window.geometry("800x600+10+20")
-
-    play_button = Button(main_window, text="Shuffle", fg="Black")
-    play_button.pack(expand=True, side = LEFT)
+    root = tk.Tk()
+    app = MusicPlayer(master=root)
+    app.mainloop()
     start = time.time()
-    #play_new_song(random_liked_song())
     end = time.time()
     print(end - start,'seconds')
-    change_volume()
-    
-def random_liked_song():
-    random_song = sp.current_user_saved_tracks(1, random.randint(0, 4150))
+
+
+
+class MusicPlayer(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.play_button = tk.Button(self, text="Shuffle", command=self.shuffle)
+        self.play_button.pack(side="left")
+        
+        self.play_button = tk.Button(self, text="Play", command=self.play)
+        self.play_button.pack(side="left")
+
+        self.pause_button = tk.Button(self, text="Pause", command=self.pause)
+        self.pause_button.pack(side="left")
+
+        self.prev_button = tk.Button(self, text="Prev", command=self.prev)
+        self.prev_button.pack(side="left")
+        
+        self.slider = tk.Scale(self, from_=0, to=100, orient="horizontal", command=self.slider_changed)
+        self.slider.pack(side="bottom")
+
+    def slider_changed(self, event):
+        print(event)
+        self.change_volume(event)
+
+
+    def shuffle(rand_song):
+        play_new_song(get_random_liked_song())
+        
+    def play(self):
+        resume_playing()
+        print("Play button pressed.")
+        
+    def pause(self):
+        print("Pause button pressed.")
+        sp.pause_playback()
+
+    def prev(self):
+        last_track()
+        print("Prev button pressed.")
+        
+    def change_volume(self, val):
+        while(True):
+            try:
+                #print('enter value to change volume')
+                vol_change = int(val)
+                sp.volume(vol_change)
+            except ValueError:
+                # print("Not an integer! Please enter an integer.")
+                continue    
+   
+
+        
+def get_random_liked_song():
+    random_song = sp.current_user_saved_tracks(1, random.randint(0, 4000))
     for i, item in enumerate(random_song['items']):
         playlist_ids = {
             'song_name': item['track']['album']['name'], 'uri': item['track']['album']['artists'][0]['uri'], 'artist_name': item['track']['album']['artists'][0]['name']}
@@ -60,24 +98,11 @@ def play_new_song(rand_song):
 def resume_playing():
     sp.start_playback()
     
-def pause():
-    sp.pause_playback()
-    
 def last_track():
     sp.previous_track()
     
-def change_volume(percent=0):
-    while(True):
-        try:
-            print('enter value to change volume')
-            vol_change = int(input())
-            sp.volume(vol_change)
-        except ValueError:
-            print("Not an integer! Please enter an integer.")
-            continue    
-            
-    
-    
+
+
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
