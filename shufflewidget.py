e017286f193a8f8137e45f7686a9085c6b973086
Isaac Brickner <bricknei@oreegonstate.edu>
2023-04-04 09:12:20 -0700

    clear old comment

--- shufflewidget.py
+++ shufflewidget.py
@@ -20,8 +20,7 @@ def main():
     app.mainloop()
     start = time.time()
     end = time.time()
-    print(end - start,'seconds')
-
+    print(end - start, 'seconds')
 
 
 class MusicPlayer(tk.Frame):
@@ -32,9 +31,10 @@ class MusicPlayer(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.play_button = tk.Button(self, text="Shuffle", command=self.shuffle)
+        self.play_button = tk.Button(
+            self, text="Shuffle", command=self.shuffle)
         self.play_button.pack(side="left")
-        
+
         self.play_button = tk.Button(self, text="Play", command=self.play)
         self.play_button.pack(side="left")
 
@@ -43,22 +43,22 @@ class MusicPlayer(tk.Frame):
 
         self.prev_button = tk.Button(self, text="Prev", command=self.prev)
         self.prev_button.pack(side="left")
-        
-        self.slider = tk.Scale(self, from_=0, to=100, orient="horizontal", command=self.slider_changed)
+
+        self.slider = tk.Scale(self, from_=0, to=100,
+                               orient="horizontal", command=self.slider_changed)
         self.slider.pack(side="bottom")
 
     def slider_changed(self, event):
         print(event)
         self.change_volume(event)
 
-
     def shuffle(rand_song):
         play_new_song(get_random_liked_song())
-        
+
     def play(self):
         resume_playing()
         print("Play button pressed.")
-        
+
     def pause(self):
         print("Pause button pressed.")
         sp.pause_playback()
@@ -66,19 +66,17 @@ class MusicPlayer(tk.Frame):
     def prev(self):
         last_track()
         print("Prev button pressed.")
-        
+
     def change_volume(self, val):
         while(True):
             try:
-                #print('enter value to change volume')
                 vol_change = int(val)
                 sp.volume(vol_change)
             except ValueError:
                 # print("Not an integer! Please enter an integer.")
-                continue    
-   
+                continue
 
-        
+
 def get_random_liked_song():
     random_song = sp.current_user_saved_tracks(1, random.randint(0, 4000))
     for i, item in enumerate(random_song['items']):
@@ -86,47 +84,21 @@ def get_random_liked_song():
             'song_name': item['track']['album']['name'], 'uri': item['track']['album']['artists'][0]['uri'], 'artist_name': item['track']['album']['artists'][0]['name']}
     return playlist_ids
 
-def play_new_song(rand_song): 
-    parse_dict = rand_song 
-    sp.start_playback(device_id=None, context_uri=parse_dict['uri'], uris=None, offset=None, position_ms=None)   
+
+def play_new_song(rand_song):
+    parse_dict = rand_song
+    sp.start_playback(
+        device_id=None, context_uri=parse_dict['uri'], uris=None, offset=None, position_ms=None)
+
 
 def resume_playing():
     sp.start_playback()
-    
+
+
 def last_track():
     sp.previous_track()
-    
 
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
-
-
-
-
-
-
-
-
-
-    
-# def get_devices():
-#     print(sp.devices())
-#     return sp.devices()
-
-
-# def get_liked_songs():
-#     multiplier = 0
-#     testarray = []
-#     ls_playlist = [] 
-#     for i in range(4200):
-#         testarray.append(sp.current_user_saved_tracks(1, multiplier))
-#         multiplier += 1    
-#     for i in range(len(testarray)):
-#         for i, item in enumerate(testarray[i]['items']):
-#             playlist_ids = {
-#                 'song_name': item['track']['album']['name'], 'uri': item['track']['album']['artists'][0]['uri'], 'artist_name': item['track']['album']['artists'][0]['name']}
-#             ls_playlist.append(playlist_ids)
-#     pp.pprint(ls_playlist)
-    
